cfd9b5d6bd6c97dae95899605e63b3c1551bc7b2
isaacjeffersonlee <27ilee01@gmail.com>
2022-08-29 18:17:45 +0100

    convert to python module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spadd",
+    description="Quick add currently playing spotify song to a user playlist with dmenu/bemenu.",
+    version="0.1",
+    packages=find_packages(),
+    scripts=["spadd/spadd.py"],
+    entry_points={
+        "console_scripts": [
+            "spadd = spadd.spadd:main",
+        ]
+    }
+)
